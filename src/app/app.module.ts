98343f4b5a4f82fae5632663244954c10416415f
Alex Coronell <alexcoronell@gmail.com>
2020-05-05 14:28:58 -0500

    Activada navegación y enlace a home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -11,6 +13,11 @@ import { StudiesComponent } from './components/studies/studies.component';
 import { TrainingComponent } from './components/training/training.component';
 import { MyworkComponent } from './components/mywork/mywork.component';
 import { ContactComponent } from './components/contact/contact.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
@@ -26,7 +33,7 @@ import { ContactComponent } from './components/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
