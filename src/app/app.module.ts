7696afe301a8e74635913f3f4a54858e3dabac26
Alex Coronell <alexcoronell@gmail.com>
2020-05-05 13:17:53 -0500

    Creados los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { StudiesComponent } from './components/studies/studies.component';
+import { TrainingComponent } from './components/training/training.component';
+import { MyworkComponent } from './components/mywork/mywork.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    FooterComponent,
+    HomeComponent,
+    ExperienceComponent,
+    StudiesComponent,
+    TrainingComponent,
+    MyworkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
